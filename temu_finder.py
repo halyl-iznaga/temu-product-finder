f9bf02de4ea8e13f32739677b021ff9dacd2ab12
halyl-iznaga <halylj01@gmail.com>
2026-03-31 14:37:16 -0400

    Update temu_finder.py

--- temu_finder.py
+++ temu_finder.py
@@ -7,7 +7,16 @@ CHAT_ID = "8440025571"
 url = "https://www.temu.com/api/search?keyword=gadgets"
 
 response = requests.get(url)
-data = response.json()
+data = {
+    "items": [
+        {"title":"Mini selladora","price":"2.5","image":"https://i.imgur.com/abc.jpg","url":"https://temu.com/product1"},
+        {"title":"Organizador de cables","price":"1.8","image":"https://i.imgur.com/def.jpg","url":"https://temu.com/product2"},
+        {"title":"Soporte celular","price":"3.2","image":"https://i.imgur.com/ghi.jpg","url":"https://temu.com/product3"},
+        {"title":"Mini linterna","price":"2.0","image":"https://i.imgur.com/jkl.jpg","url":"https://temu.com/product4"},
+        {"title":"Batería portátil","price":"5.5","image":"https://i.imgur.com/mno.jpg","url":"https://temu.com/product5"},
+        {"title":"Llavero multifunción","price":"0.99","image":"https://i.imgur.com/pqr.jpg","url":"https://temu.com/product6"}
+    ]
+}
 
 products = []
 
