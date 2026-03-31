d4dfa7159e227c3cfaf246c40ee5419939c5f42b
halyl-iznaga <halylj01@gmail.com>
2026-03-31 14:28:55 -0400

    Update temu_finder.py

--- temu_finder.py
+++ temu_finder.py
@@ -1,8 +1,8 @@
 import requests
 import random
 
-TOKEN = "TU_TOKEN"
-CHAT_ID = "TU_CHAT_ID"
+TOKEN = "8759419199:AAGJtdCyd1Q7rUZQ2ZNA3_CuHG_IV-lCElg"
+CHAT_ID = "8440025571"
 
 url = "https://www.temu.com/api/search?keyword=gadgets"
 
